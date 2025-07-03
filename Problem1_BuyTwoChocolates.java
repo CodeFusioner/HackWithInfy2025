c962e9d62d166f1919226ded63771ada2c47170e
Kapil Gupta <kg666235@gmail.com>
2025-07-03 10:07:40 +0530

    first commit

--- Problem1_BuyTwoChocolates.java
+++ Problem1_BuyTwoChocolates.java
@@ -2,4 +2,15 @@ public class Problem1_BuyTwoChocolates {
     public static void main(String[] args) {
 
     }
+    public int buyChoco(int[] prices, int money) {
+        Arrays.sort(prices);
+        int smallest1 = prices[0];
+        int secondSmallest = prices[1];
+        int sum = smallest1 + secondSmallest;
+        int ans = money - sum;
+        if(sum > money){
+            return money;
+        }
+        return ans;
+    }
 }
