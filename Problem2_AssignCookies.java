7e0b8008a59303e5ff91e95e157892be4d995329
Kapil Gupta <kg666235@gmail.com>
2025-07-03 10:09:13 +0530

    first commit

--- Problem2_AssignCookies.java
+++ Problem2_AssignCookies.java
@@ -1,2 +1,18 @@
 public class Problem2_AssignCookies {
+    public static void main(String[] args) {
+
+    }
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int i=0;
+        int j=0;
+        while(i<s.length && j<g.length){
+            if(g[j] <= s[i]){
+                j++;
+            }
+            i++;
+        }
+        return j;
+    }
 }
